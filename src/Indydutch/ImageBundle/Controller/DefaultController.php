fe8935cb898872542f64f4155715ba1ed614f762
eddy <eddy147@gail.com>
2015-01-30 09:38:25 +0100

    SonataAdmin

--- src/Indydutch/ImageBundle/Controller/DefaultController.php
+++ src/Indydutch/ImageBundle/Controller/DefaultController.php
@@ -7,12 +7,17 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    /**
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction(Request $request)
     {
         $categories = $this->getDoctrine()
             ->getRepository('IndydutchImageBundle:Category')
             ->findAll();
-        
+
         return $this->render(
             'IndydutchImageBundle:Default:index.html.twig',
             array(
